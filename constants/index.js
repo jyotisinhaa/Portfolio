fff1db7e3f1fa1f4eea91721c802bc0657a6c73b
Jyoti Sinha <jyotisinhabit@gmail.com>
2026-01-19 12:11:24 -0800

    Removed blog section

--- constants/index.js
+++ constants/index.js
@@ -1,8 +1,7 @@
 import { AppsData } from "./AppsData";
-import { BlogsData } from "./BlogsData";
 import { NewsData } from "./NewsData";
 import { SkillsData } from "./SkillsData";
 import { JourneyData } from "./JourneyData";
 import { ProjectsData } from "./ProjectsData";
 
-export { AppsData, BlogsData, NewsData, SkillsData, JourneyData, ProjectsData }
+export { AppsData, NewsData, SkillsData, JourneyData, ProjectsData };
